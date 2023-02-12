7234be2cbc97b26f135f6fe8592fd0efea8631d6
faizannazir <faizannazir289@gmail.com>
2023-02-12 23:05:22 +0500

    update css

--- admin/js/attendance.js
+++ admin/js/attendance.js
@@ -13,35 +13,46 @@ document.addEventListener("DOMContentLoaded", () => {
     form.addEventListener("submit", (event) => {
         event.preventDefault(); // stop the form from submitting
 
+        if(id.value.trim() =="")
+        {
+            id.style.borderColor="red";
+            error.innerHTML = "please enter id";
+            return;
+        }
         if (id.value.trim() <=0) {
             id.style.borderColor="red";
             error.innerHTML = "please enter valid id";
             return;
         }
+        id.style.borderColor = "#00ce9f";
         
         if (startDateInput.value == "" || endDateInput.value == "") {
             error.innerHTML = "Both fields are requireds ";
             return;
         }
         
-
         if ((new Date(startDateInput.value).toLocaleDateString().padStart(10, "0")) > today) {
+            startDateInput.style.borderColor = "red";
             error.innerHTML = "Start date cannot be after today  date " + today;
             return;
-
         }
+
         startDateInput.style.borderColor="#00ce9f";
         if ((new Date(endDateInput.value).toLocaleDateString().padStart(10, "0")) > today) {
+            endDateInput.borderColor = "red";
             error.innerHTML = "End date cannot be after today date " + today;
             return;
 
-        }
-        error.innerHTML = "";
+        }   
+        endDateInput.style.borderColor = "#00ce9f";
+    
 
         if (startDateInput.value > endDateInput.value) {
+            startDateInput.style.borderColor = "red";
             error.innerHTML = "End date cannot be before start date!";
             return;
         }
+        startDateInput.style.borderColor="#00ce9f";
         error.innerHTML = "";
 
         form.submit();
